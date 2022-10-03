83c56f98f079a975da38c283a7985ede090f658b
devtiagomantay <devtiagomantay@gmail.com>
2022-10-03 01:39:57 +0100

    Create function to call gutendex api with the bookname as pathparameter

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
 from flask import Flask
+import requests
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def test_endpoint():
- return 'It worked!'
+@app.route('/books/name/<bookname>')
+def test_endpoint(bookname):
+    return request_gutendex(bookname)
+
+
+def request_gutendex(bookname):
+    uri = 'https://gutendex.com/books?search=' + bookname
+    response = requests.get(uri)
+    return response.json()
 
 
 if __name__ == '__main__':
- app.run()
+    app.run()
+
