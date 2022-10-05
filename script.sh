11178bcc23daff02ee48c88c377ee132f1f28335
devtiagomantay <devtiagomantay@gmail.com>
2022-10-05 05:35:15 +0100

    Fix docker compose issue

--- script.sh
+++ script.sh
@@ -3,7 +3,7 @@ sudo apt install python3 virtualenv docker docker-compose git
 python3 -m virtualenv venv
 source venv/bin/activate
 pip install -r requirements.txt
-sudo docker-compose up
+sudo docker-compose -f docker-compose.yaml up -d
 export DATABASE_URL="mysql+pymysql://root:@localhost:3306/database"
 flask db init
 flask db migrate
